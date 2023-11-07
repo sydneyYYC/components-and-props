2a70e9f52d1596c8277e5e9dc253ed08fe79b70f
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-07 13:12:37 -0700

    added navbar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let navItems = [
+    {name: 'home', href:"/"},
+    {name: 'gallery', href:"/gallery"},
+     {name:'about', href:"/about"}
+    ]
+</script>
+
+<!-- TODO add hover -->
+<header class="max-w-full flex justify-between items-center bg-slate-900 text-violet-100">
+  <div class="p-2 hover:violet-500"> Logo </div>
+  <nav>
+  <ul class="flex g-2">
+    {#each navItems as {name, href}}
+    <li class="p-2"><a href={href}>{name}</a></li>
+    {/each}
+  </ul>
+</nav>
+</header>
